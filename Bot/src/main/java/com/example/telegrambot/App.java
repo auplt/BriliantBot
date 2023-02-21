597d2f70acce81a364e1c230667ba8d4f8612b2c
auplt <98539159+auplt@users.noreply.github.com>
2023-02-21 14:46:45 +0300

    CBFS-3 MarkCommand added

--- Bot/src/main/java/com/example/telegrambot/App.java
+++ Bot/src/main/java/com/example/telegrambot/App.java
@@ -1,6 +1,7 @@
 package com.example.telegrambot;
 
 import com.example.telegrambot.bot.Bot;
+import org.telegram.telegrambots.bots.DefaultBotOptions;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
@@ -8,11 +9,12 @@ import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 public class App {
     public static void main(String[] args) {
         try {
-            TelegramBotsApi botsApi =
-                    new TelegramBotsApi(
-                            DefaultBotSession.class);
-            botsApi.registerBot(new Bot());
-            System.out.println("YES");
+
+            TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
+            botsApi.registerBot(new Bot(new DefaultBotOptions()));
+
+            System.out.println("ONLINE");
+
         } catch (TelegramApiException e) {
             e.printStackTrace();
         }
