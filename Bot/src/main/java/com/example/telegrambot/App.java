3c729c9b347db2034329dbfc8116110ce7111a35
auplt <98539159+auplt@users.noreply.github.com>
2023-02-21 16:29:20 +0300

    Revert "CBFS-5-add-mark-command"

--- Bot/src/main/java/com/example/telegrambot/App.java
+++ Bot/src/main/java/com/example/telegrambot/App.java
@@ -1,7 +1,6 @@
 package com.example.telegrambot;
 
 import com.example.telegrambot.bot.Bot;
-import org.telegram.telegrambots.bots.DefaultBotOptions;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
@@ -9,12 +8,11 @@ import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 public class App {
     public static void main(String[] args) {
         try {
-
-            TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
-            botsApi.registerBot(new Bot(new DefaultBotOptions()));
-
-            System.out.println("ONLINE");
-
+            TelegramBotsApi botsApi =
+                    new TelegramBotsApi(
+                            DefaultBotSession.class);
+            botsApi.registerBot(new Bot());
+            System.out.println("YES");
         } catch (TelegramApiException e) {
             e.printStackTrace();
         }
