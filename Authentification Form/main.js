9b89ba45183af779dcacc84bae5b98471d4ce1f7
Rina-01 <ira.mashkova.2001@mail.ru>
2023-02-28 15:19:46 +0300

    Update main.js
    silly error

--- Authentification Form/main.js
+++ Authentification Form/main.js
@@ -9,16 +9,16 @@ function getData() {
         spanAlertL.textContent = 'Логин должжен быть длинне 3 символов';
         //alert('Login must contain at least 3 characters');
     } else {
-        spanAlertL.textContent = 'Ваш логин: ' + res;
-        //alert(res);
+        spanAlertL.textContent = 'Ваш логин: ' + resL;
+        //alert(resL);
     }
 
     if (resP.length < 3) {
         spanAlertP.textContent = 'Пароль должжен быть длинне 3 символов';
         //alert('Password must contain at least 3 characters');
     } else {
-        spanAlertP.textContent = 'Ваш пароль: ' + res;
-        //alert(res);
+        spanAlertP.textContent = 'Ваш пароль: ' + resP;
+        //alert(resP);
     }
 }
 
