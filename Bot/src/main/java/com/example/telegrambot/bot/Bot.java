66530253090edd24f6ce38f4910bdeccba527205
auplt <98539159+auplt@users.noreply.github.com>
2023-03-10 14:03:06 +0300

    Revert "Merge branch 'CBFS-8-WebForm' into CBFS-3-init"
    This reverts commit 8d7868153f8c3e3c2035dbd10e24880d69db1d72, reversing
    changes made to ed7e50f72f349194a2ab55e259410023e7135446.

--- Bot/src/main/java/com/example/telegrambot/bot/Bot.java
+++ Bot/src/main/java/com/example/telegrambot/bot/Bot.java
@@ -1,6 +1,10 @@
 package com.example.telegrambot.bot;
 
-import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import com.example.telegrambot.bot.command.HelpCommand;
+import com.example.telegrambot.bot.command.MarkCommand;
+import com.example.telegrambot.bot.command.StartCommand;
+import org.telegram.telegrambots.bots.DefaultBotOptions;
+import org.telegram.telegrambots.extensions.bots.commandbot.TelegramLongPollingCommandBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
@@ -19,21 +23,28 @@ public class Bot extends TelegramLongPollingCommandBot {
 
     @Override
     public String getBotUsername() {
-        return "HelpToPlanStudyBot";
+        return BOT_NAME;
     }
 
     @Override
     public String getBotToken() {
-        return "6015366458:AAHOfVyAH1zFCRIkmIgEBLh2artNvpntfTw";
+        return BOT_TOKEN;
+    }
+
+    public String getBotName() {
+        return BOT_NAME;
+    }
+
+    public void processInvalidCommandUpdate(Update update) {
+        System.out.println("Invalid Command");
     }
 
     @Override
-    public void onUpdateReceived(Update update) {
+    public void processNonCommandUpdate(Update update) {
         // We check if the update has a message and the message has text
         if (update.hasMessage() && update.getMessage().hasText()) {
             SendMessage message = new SendMessage(); // Create a SendMessage object with mandatory fields
             message.setChatId(update.getMessage().getChatId().toString());
-            message.setText(update.getMessage().getText());
 
             String ReceivedText = update.getMessage().getText() + " T_T";
 
