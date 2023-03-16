d93e64be8961268ba503ef251e7f645b38bc6587
Rina-01 <ira.mashkova.2001@mail.ru>
2023-03-16 09:36:00 +0300

    CBFS-10
    form 2.0

--- Authentification Form/main.js
+++ Authentification Form/main.js
@@ -1,25 +1,20 @@
 function getData() {
     var resL = document.getElementById('login').value;
-    var spanAlertL = document.getElementById('inLogin');
-
     var resP = document.getElementById('password').value;
-    var spanAlertP = document.getElementById('inPassword');
+    
+  var pAlert = document.getElementById('forErrors');
+  pAlert.textContent = "";
 
     if (resL.length < 3) {
-        spanAlertL.textContent = 'Логин должжен быть длинне 3 символов';
+        pAlert.textContent += 'Логин должен быть длинне 3 символов';
         //alert('Login must contain at least 3 characters');
-    } else {
-        spanAlertL.textContent = 'Ваш логин: ' + resL;
-        //alert(resL);
-    }
+    } 
 
     if (resP.length < 3) {
-        spanAlertP.textContent = 'Пароль должжен быть длинне 3 символов';
+        pAlert.textContent += '\nПароль должен быть длинне 3 символов';
         //alert('Password must contain at least 3 characters');
-    } else {
-        spanAlertP.textContent = 'Ваш пароль: ' + resP;
-        //alert(resP);
-    }
+    } 
+  
 }
 
 
--- Bot/README.md
+++ Bot/README.md
@@ -6,7 +6,15 @@ Base for working Telegram Bot
 Starts the bot on the specified parameters "bot name" and "token"
 
 ## Class Bot.java
-- Override basic methods of TelegramLongPollingBot.
+- Override basic methods of TelegramLongPollingCommandBot.
 - implement command botConnect. Register selected bot in TelegramAPI
 - When receiving update only logged the event is not taking any action.
+
+
+
+# Command List
+auth - authtorise
+help - list all known commands
+mark - get your average mark
+start - start using bot
 
